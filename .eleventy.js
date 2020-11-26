de951c9807d336d565cf8b20365f5ab9766d6e2d
Tatsuya Kinno <kinnou@mefilas.com>
2020-11-26 23:54:07 +0900

    minifyあり

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
 const htmlmin = require('html-minifier')
 
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
-  //   if (outputPath.endsWith('.html')) {
-  //     const minified = htmlmin.minify(content, {
-  //       collapseWhitespace: true,
-  //       minifyCSS: true,
-  //       minifyJS: true,
-  //       removeComments: true
-  //     })
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      const minified = htmlmin.minify(content, {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true
+      })
 
-  //     return minified
-  //   }
+      return minified
+    }
 
-  //   return content
-  // })
+    return content
+  })
 
   return {
     templateFormats: ['njk'],
